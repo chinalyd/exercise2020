37654bdace8fee8c7b36a4ba476c59bcb4c1a4a1
chinalyd <chinalyd0820@sina.cn>
2020-06-04 18:17:43 +0800

    新文件: classtest_3.py

---
+++ classtest_3.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+class UserData(object):
+    def __init__(self, id, name):
+        self.id = id
+        self._name = name
+
+class NewUser(UserData):
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        if len(value) > 3:
+            self._name = value
+        else:
+            print('ERROR')
+
+    def __repr__(self):
+        return '{}'.format(self._name)
+
+if __name__ == '__main__':
+    user1 = NewUser(101, 'Jack')
+    user1.name = 'Lou'
+    user1.name = 'Jackie'
+    user2 = NewUser(102, 'Louplus')
+    print(user1)
+    print(user2)
+    
