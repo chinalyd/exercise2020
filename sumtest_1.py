56642451999f987621a8c5d96f31538cc245eedc
chinalyd <chinalyd0820@sina.cn>
2020-06-07 05:31:14 +0800

    f-Strings

---
+++ sumtest_1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+first = int(input("Please enter the first num:"))
+second = int(input("Please enter the second num:"))
+third = int( input("Please enter the third num:"))
+sum = first + second + third
+
+f"a = {first}, b = {second}, c = {third}, a+b+c = {sum}"
+
